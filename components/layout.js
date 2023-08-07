cbb087dc768e336d82002be10e5e60e8183a96d4
Cloud Dark <clouddark1798@gmail.com>
2023-08-07 18:26:39 +0700

    final clientside rendering

--- components/layout.js
+++ components/layout.js
@@ -1,15 +1,14 @@
+import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
-// import styles from '../styles/alert.module.css';
 import { clsx } from 'clsx';
-const name = 'Syahdan';
-import { useState, useEffect } from 'react'
 import moment from 'moment';
-const currentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
-export const siteTitle = 'Next.js Sample Website';
+
+const name = 'Syahdan';
+const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home, type }) {
   const [isClient, setIsClient] = useState(false)
@@ -17,9 +16,19 @@ export default function Layout({ children, home, type }) {
   useEffect(() => {
     setIsClient(true)
   }, [])
-  return (
+  const [currentDate, setCurrentDate] = useState(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
-   
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentDate(moment().format('MMMM Do YYYY, h:mm:ss a'));
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
     <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
@@ -30,7 +39,7 @@ export default function Layout({ children, home, type }) {
         <meta
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle,
+            siteTitle
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
         <meta name="og:title" content={siteTitle} />
@@ -52,41 +61,46 @@ export default function Layout({ children, home, type }) {
         ) : (
           <>
             <Link href="/">
-              <Image
-                priority
-                src="/images/profile.jpg"
-                className={utilStyles.borderCircle}
-                height={108}
-                width={108}
-                alt=""
-              />
+              <a>
+                <Image
+                  priority
+                  src="/images/profile.jpg"
+                  className={utilStyles.borderCircle}
+                  height={108}
+                  width={108}
+                  alt=""
+                />
+              </a>
             </Link>
             <h2 className={utilStyles.headingLg}>
-              <Link href="/" className={utilStyles.colorInherit}>
-                {name}
+              <Link href="/">
+                <a className={utilStyles.colorInherit}>{name}</a>
               </Link>
             </h2>
-            <div className={clsx({
-            [styles.success]: type === 'success',
-            [styles.error]: type === 'error',
-            })}
-        >
-        {children}
-      </div>
+            <div
+              className={clsx({
+                [styles.success]: type === 'success',
+                [styles.error]: type === 'error',
+              })}
+            >
+              {children}
+            </div>
           </>
         )}
       </header>
       <main>{children}</main>
-      {!home && (
+      {!home &&(
         <div className={styles.backToHome}>
-          <Link href="/">← Back to home</Link>
+          <Link href="/">
+            <a>← Back to home</a>
+          </Link>
         </div>
       )}
+      {home && isClient&& (
       <footer className={styles.footer}>
-        {home &&isClient&& (
-              <span>{currentDate}</span>
-        )}
+      <span>{currentDate}</span>
       </footer>
+      )}
     </div>
   );
 }
